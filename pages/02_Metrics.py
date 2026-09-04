8118bedbcee6e877ea985bede3cf3574e9c4249f
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-09-04 16:10:52 +0100

    Create Metrics page with financial metrics display
    Added a Streamlit app for displaying financial metrics.

---
+++ pages/02_Metrics.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Metrics")
+
+st.markdown("""
+Calcul automatique :
+
+- Perf YTD
+- Perf annualisée
+- Sharpe
+- Treynor
+- Information Ratio
+- Beta
+- Tracking Error
+""")
